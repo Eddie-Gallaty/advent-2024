643127f07ef74505fd0e82c4d7db195a645953c2
Eddie-Gallaty <eddiegallaty@live.com>
2024-12-02 22:02:45 -0500

    working thru part 2

--- day-one/one.py
+++ day-one/one.py
@@ -14,17 +14,61 @@ print(len(list_content))
 list1.sort()
 list2.sort()
 
-
 a = 0 
 z = [ int(x) - int(y) for x, y in zip(list1,list2)]
 
-
+#this is part ones answer
 for b in z:
     a+= abs(b)
-
-for i in list1:
-    print(i)
 print(a)
+
+#part 2
+
+#for x in list1:
+   # print(list2.count(x))
+
+my_dict1 = dict(enumerate(list1))
+my_dict2 = dict(enumerate(list2))
+
+for value in my_dict1.values():
+    print(value)
+
+testlist1 = [1, 2, 2 ,2, 3, 3, 4, 0]
+testlist2 = [1, 2, 2 ,2, 3, 4, 4, 0]
+
+testdict1 = dict(enumerate(testlist1))
+testdict2 = dict(enumerate(testlist2))
+
+#for key in testdict1.keys():
+ #   if key in testdict2 and testdict1[key] == testdict2[key]:
+#        print(testdict2[key])
+
+
+
+alpha = 0
+beta = 0
+omega = 0
+zeta = 0
+for key in testdict1.keys():
+    print("starting " + str(alpha))
+    if key in testdict2 and testdict1[key] == testdict2[key]:
+        alpha += 1
+        zeta = key
+        print(str(alpha) + " first if")
+    if key != zeta:
+        print("hello")
+        alpha = 0
+        print(str(alpha) + " 2nd if")
+    else:
+        beta = key * alpha
+        print(str(beta) + " this is beta")
+        omega += beta
+        alpha = 0
+        print(str(alpha) + " 3rd if")
+    
+    print(omega)
+print(omega)
+
 
 #for x in list_content:
 #    print(x)
