223be51093fdb918ebca38bd3dba6e78f9004d11
Eddie-Gallaty <eddiegallaty@live.com>
2025-01-10 13:06:21 -0500

    adding stuff

--- day-two/day-two-alt/day_two.py
+++ day-two/day-two-alt/day_two.py
@@ -2,52 +2,35 @@ reports = []
 with open("num.txt", 'r') as f:
     for line in f.readlines():
         reports.append(line.strip().split())
-    #print(reports)
+
 test_list = ["1", "4", "3", "4", "5"]
 test_list1 = ["5", "4", "3", "2", "1"]
 test_list2 = ["5", "6", "7", "50", "1"]
 test_list3 = ["5", "4", "3", "2", "8"]
 
-is_inc = 0
-is_desc = 0
-not_inc = 0
-not_desc = 0
-neither = 0
-notSafeAbs = 0
-num_reports = len(reports)
+reports = [[int(i) for i in report] for report in reports] #jesus help me
 
-
-def isSafe(report):#, is_inc, is_desc, not_inc, not_desc, neither, notSafeAbs):
-    #report = int(report)
+def isSafe(report, num_reports):
+    
+    safe = True
+   
     isIncreasing = all((x < y for x, y in zip(report, report[1:])))
     isDecreasing = all ((x > y for x, y in zip(report, report[1:])))
-    for x in report in range(len(report) -1):
-        print(x)
-        #abs_num = int(report[x]) - int(report[x + 1])
-      # abs_num = abs(abs_num)
-       # y = abs(x) - abs(x+1)
-       # y = abs(y)
-       # print(str(abs_num))
-       
-        #if abs_num < 1 or abs_num >= 3:
-           #print("here")
 
-        return False
+    for x  in range(len(report) -1):
+     #   print(report[x])
+      # report = [int(i) for i in report] #casting all elements in report to int <--- WRONG WRONG WRONG WRONG WRONG 
+       abs_diff = abs(report[x] - abs(report[x + 1]))
+       if abs_diff < 1 or abs_diff > 3:
+          safe = False
+
+    if safe and (isIncreasing or isDecreasing):
+       num_reports += 1
+
+    return num_reports
 
 for report in reports:
   #  print("testing " +str(isSafe(report)))
-    if not isSafe(report):
-        num_reports += 1
+  num_reports = isSafe(report, num_reports)
 
 print(num_reports)
-
-
-    
-
-#print("There are "+str(is_inc)+" reports that are increasing")
-#print("There are "+str(is_desc)+" reports that are decreasing")
-#print("There are "+str(not_inc)+" reports that are NOT increasing")
-#print("There are "+str(not_desc)+" reports that are NOT decreasing")
-#print("There are "+str(neither)+" reports that are NEITHER increasing nor decreasing")
-#print("There are "+str(notSafeAbs)+" reports that are NOT safe abs");
-#print("testing " +str(isSafe(test_list1)))
--- day-two/two.py
+++ day-two/two.py
@@ -7,22 +7,19 @@ with open("numbers.txt", 'r') as f:
 safe_reports = len(reports)
 
 for report in reports:
-    report = [int(x) for x in report] # blanket casting int 
-    isSafe = True # reset to true each start of a new iteration
-    is_increasing = all(report[a] < report[a+1] for a in range(len(report) -1))  #increase check 
-    is_decreasing = all(report[a] > report[a+1] for a in range(len(report) -1))  #decrease check (all is a cool function)
+    report = [int(x) for x in report]  
+    isSafe = True 
+    is_increasing = all(report[a] < report[a+1] for a in range(len(report) -1))
+    is_decreasing = all(report[a] > report[a+1] for a in range(len(report) -1))    
     
-    #this bit took me about 6 hours of troubleshooting and reading docs. i deleted 4 different ways of trying before i landed on this
-    for x in range(len(report)-1): #this is looping through each of the numbers of the report. 
-        abs_diff = abs(report[x] - abs(report[x+1])) # getting the difference between the index and its neighbor (going right)
-        if abs_diff < 1 or abs_diff > 3: #if it is not within the ranges, set it to false. equal to or greater than 1 and less than or equal to 3
-            isSafe = False #set this bad boy to false
-            break # jumping to the next as soon as it fails the compare check
-    if isSafe and (is_increasing or is_decreasing):  #bool checks
+    for x in range(len(report)-1): 
+        abs_diff = abs(report[x] - abs(report[x+1]))
+        if abs_diff < 1 or abs_diff > 3:
+            isSafe = False 
+            break 
+    if isSafe and (is_increasing or is_decreasing): 
         print("Hello World")
         print(report)
     else:
-        safe_reports -= 1     #minus one from the safe reports list is report fails the bool checks
-    
-#Answer for part one :)           
+        safe_reports -= 1         
 print("number of safe reports after counting "+str(safe_reports))
