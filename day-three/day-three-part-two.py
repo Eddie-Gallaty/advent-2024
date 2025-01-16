b2cdc0519d24c2e21618ed6070c67f1c4f777429
Edward Gallaty <edwardgallaty@Edwards-MacBook-Pro.local>
2025-01-15 23:21:28 -0500

    created regex for first part

--- day-three/day-three-part-two.py
+++ day-three/day-three-part-two.py
@@ -1,7 +1,14 @@
 import re
-text = "from()%mul(296,219)what()select()mul(684,974{where()where()how()~(,{who()mul(106,612)!why()select()select(533,857)who()/^%mul(465,917)?how()do()/?%why()mul(901): select(646,127)mul(395,446)/+,mul(883,551)@'&,?, ?mul(840,262)mul(593,805)from()<~{why(302,32)@do():what()!mul(458,211)how()$mul(804,650)-)mul(708,773)~~do(){',,^why())$why()/mul(987,695)^mul(130,150)mul(311,404)who()what()-do()~~where()why()"
-pattern = re.compile(r'\bdo\(\)')
+text = "don't()7297jhmul(23,23)jskicnbdo()kahjsdmnbmul(4,3){"
+pattern = re.compile(r"(?:don't\(\).*?mul\(\d+,\d+\))|(?<!don't\(\))(mul\(\d+,\d+\)|do\(\))")
 
+with open("puzzle.txt") as f:
+    text = f.read()
+    results = pattern.findall(text)
+eval_num = 0
+rep_mul = []
+
+regex = re.compile(r'mul\(\d{1,3},\d{1,3}\)') #gross
 #1 find all the do
 #2 figure out how to also include the mul(,)
 #3 add to list
@@ -11,5 +18,6 @@ pattern = re.compile(r'\bdo\(\)')
 #7 profit
 
 reg = pattern.findall(text)
+new_list = [item for item in reg if item != "" and item != "do()"]
 
-print(reg)
+print(new_list)
